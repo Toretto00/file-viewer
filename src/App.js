839382cb8ab5f3ffa97ceb52573c539db183f7fe
baophan0207 <bao@anygen.ai>
2025-01-14 10:52:02 +0700

    No changes detected in the current diff.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import FileViewer from "./FileViewer/FileViewer";
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <FileViewer />
+      </div>
+    );
+  }
+}
+
+export default App;
